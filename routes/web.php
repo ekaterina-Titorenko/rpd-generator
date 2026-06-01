eadf1d4eca9b2c7558bb40a99e2a6080a459728a
katerina-Evdokimova <eea2002_2107leto@mail.ru>
2026-06-01 14:11:23 +0300

    Add RPD curriculum routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\RpdCurriculumItemController;
 use App\Http\Controllers\RpdProgramController;
 use Illuminate\Support\Facades\Route;
 
@@ -8,3 +9,19 @@ Route::get('/', function () {
 });
 
 Route::resource('rpd-programs', RpdProgramController::class);
+
+Route::prefix('rpd-programs/{rpdProgram}')
+    ->name('rpd-programs.')
+    ->group(function () {
+        Route::get('curriculum', [RpdCurriculumItemController::class, 'index'])
+            ->name('curriculum.index');
+
+        Route::post('curriculum', [RpdCurriculumItemController::class, 'store'])
+            ->name('curriculum.store');
+
+        Route::put('curriculum/{curriculumItem}', [RpdCurriculumItemController::class, 'update'])
+            ->name('curriculum.update');
+
+        Route::delete('curriculum/{curriculumItem}', [RpdCurriculumItemController::class, 'destroy'])
+            ->name('curriculum.destroy');
+    });
