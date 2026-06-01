129cfae5e48bcaddc3af5f32195135b21f84a5bc
katerina-Evdokimova <eea2002_2107leto@mail.ru>
2026-06-01 13:30:52 +0300

    Add RPD program routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\RpdProgramController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('rpd-programs.index');
 });
+
+Route::resource('rpd-programs', RpdProgramController::class);
